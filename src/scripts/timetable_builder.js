1304e5d55435ed12cf2f3954cac785e04eaf94d8
TrpFrog <dev@trpfrog.net>
2022-01-26 04:17:40 +0900

    Refactor lectureDataToHTML

--- src/scripts/timetable_builder.js
+++ src/scripts/timetable_builder.js
@@ -44,32 +44,43 @@ function idPassButton(id, pass) {
                         class="timetable-button"><i class="fas fa-id-card"></i></span>`;
 }
 
-function lecture_data_to_html(lecture) {
+function lectureDataToHTML(lecture) {
     let needSlash = lecture.teacher !== '' && lecture.type !== '';
     let windowColor = lecture.color === ''  ? '' : `background-color: ${lecture.color};`;
-    
-    let html = `<div class="timetable-lecture-shell" style="${lectureDataToCSS(lecture)}">
-        <div class="timetable-period-mobile-shell">`;
-    
-    for(let i = 0; i < lecture.lect_length; i++) {
-        html += `
-            <div class="timetable-heading timetable-period-mobile centering">
-                <div class="timetable-element-wrapper">
-                    <p>${time[lecture.period - 1 + i]}</p>
-                </div>
-            </div>`;
-    }
 
-    html += `
-        </div>
+    return `
+        <div class="timetable-lecture-shell" style="${lectureDataToCSS(lecture)}">
+        
+            <!-- Time notation for smartphones --->
+            <div class="timetable-period-mobile-shell">
+                ${(() => {
+                    // Generate time notations for the class length.
+                    let smartphoneTimeNotation = '';
+                    for(let i = 0; i < lecture.lect_length; i++) {
+                        smartphoneTimeNotation += `
+                        <div class="timetable-heading timetable-period-mobile centering">
+                            <div class="timetable-element-wrapper">
+                                <p>${time[lecture.period - 1 + i]}</p>
+                            </div>
+                        </div>`;
+                    }
+                    return smartphoneTimeNotation;
+                })()}
+            </div>
+            
+            <!-- Lecture block --->            
             <div class="timetable-element centering" style="${windowColor}">
                 <div class="timetable-element-wrapper">
-                    <p class="lecture-name">${lecture.name}</p>
-                    <p>${lecture.teacher}${needSlash ? ' / ' : ''}${lecture.type}</p>
+                    <p class="lecture-name">
+                        ${lecture.name}
+                    </p>
+                    <p>
+                        ${lecture.teacher}${needSlash ? ' / ' : ''}${lecture.type}
+                    </p>
                     <div style="display: inline-block;">
                         <p style="margin: 0;">
-                            ${Object.keys(SPECIFIC_FONT_AWESOME_CLASSES).map(e => 
-                                buttonComponent(lecture[e], e)    
+                            ${Object.keys(SPECIFIC_FONT_AWESOME_CLASSES).map(e =>
+                                buttonComponent(lecture[e], e)
                             ).join('\n')}
                             ${zoomIdPassButton(lecture.zoom_id, lecture.zoom_password)}
                             ${idPassButton(lecture.id, lecture.password)}
@@ -77,11 +88,9 @@ function lecture_data_to_html(lecture) {
                     </div>
                 </div>
             </div>
-        </div>`;
-    
-    html += '</div></div>'
-
-    return html;
+            
+        </div>
+    `;
 }
 
 function getDividedTimetable() {
@@ -96,37 +105,53 @@ function getDividedTimetable() {
     return alignedTimetable;
 }
 
-function init_timetable() {
-    const lects = getDividedTimetable();
-    const table = document.getElementById('timetable');
-    document.getElementById('header_wrapper').innerHTML = '<h1>' + page_title + '</h1>';
+function setPageTitle(pageTitle) {
+    document.getElementById('header_wrapper').innerHTML = '<h1>' + pageTitle + '</h1>';
+}
 
-    const theme = (typeof color_theme === 'undefined' || color_theme === '') ? 'color.css' : color_theme;
+function setPageTheme(colorTheme) {
+    const theme = (typeof colorTheme === 'undefined' || colorTheme === '') ? 'color.css' : colorTheme;
     const themeCode = `@import "${theme}";`
     document.getElementsByTagName('style')[0].insertAdjacentHTML('afterbegin', themeCode);
+}
+
+function initTimetable() {
+    const lecturesInDay = getDividedTimetable();
+
+    setPageTitle(page_title);
+    setPageTheme(color_theme);
+
+    const tableElement = document.getElementById('timetable');
 
     for (let i in day_of_weeks) {
-        const html = `<div class="timetable-heading centering" style="grid-column: ${2 + parseInt(i)}; grid-row: 1">    
-            <div class="timetable-element-wrapper">
-                <p>${day_of_weeks[i]}</p>
-            </div>
-        </div>`;
-        table.insertAdjacentHTML('beforeend', html);
-        for(const lecture of lects[i]) {
-            table.insertAdjacentHTML('beforeend', lecture_data_to_html(lecture));
+        const className = 'timetable-heading centering';
+        const style = `grid-column: ${2 + parseInt(i)}; grid-row: 1`;
+        const html = `
+            <div class="${className}" style="${style}">    
+                <div class="timetable-element-wrapper">
+                    <p>${day_of_weeks[i]}</p>
+                </div>
+            </div>`;
+        tableElement.insertAdjacentHTML('beforeend', html);
+
+        for(const lecture of lecturesInDay[i]) {
+            tableElement.insertAdjacentHTML('beforeend', lectureDataToHTML(lecture));
         }
     }
 
     for(const i in time) {
-        const html = `<div class="timetable-heading timetable-period-pc centering" style="grid-column: 1; grid-row: ${2 + parseInt(i)}">
-            <div class="timetable-element-wrapper">
-                <p>${time[i]}</p>
-            </div>
-        </div>`;
-        table.insertAdjacentHTML('beforeend', html);
+        const className = 'timetable-heading timetable-period-pc centering';
+        const style = `grid-column: 1; grid-row: ${2 + parseInt(i)}`;
+        const html = `
+            <div class="${className}" style="${style}">
+                <div class="timetable-element-wrapper">
+                    <p>${time[i]}</p>
+                </div>
+            </div>`;
+        tableElement.insertAdjacentHTML('beforeend', html);
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    init_timetable();
+    initTimetable();
 });
